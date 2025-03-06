af01b4c2c348644a421499cfebb12e0e6e71a5ca
jordan-furr <jordanfurr98@gmail.com>
2025-03-06 11:53:37 +0100

    outline

---
+++ app/components/header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import styles from "./header.module.css";
+
+export default async function Header() {
+    return (
+        <div className={styles.headerCont}>
+            <h1 className={styles.hopeTitle}>
+                Hope Accelerator
+            </h1>
+            <div className={styles.menuCont}>
+                <Image
+                    src="/Hope-Accelerator-Retreats-Normandy-France-w.PNG"
+                    alt="Menu"
+                    width={60}
+                    height={60}
+                />
+            </div>
+        </div>
+    );
+}
