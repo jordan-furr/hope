5cf4a5f88d65e72c145e8a3551ef2afbb50cd668
jordan-furr <jordanfurr98@gmail.com>
2025-03-19 10:40:24 +0100

    image

--- app/components/subscribe-form.tsx
+++ app/components/subscribe-form.tsx
@@ -6,6 +6,9 @@ export default async function SubscribeForm() {
             <div>
                 <p>Email</p>
             </div>
+            <div>
+                <p>Subscribe</p>
+            </div>
         </div>
     );
 }
