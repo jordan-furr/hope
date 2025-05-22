7494394d6dfe2cfcd1bdeb202c502b2dab8738aa
jordan-furr <jordanfurr98@gmail.com>
2025-05-22 16:42:11 -0600

    new pic

--- app/components/subscribe-form.tsx
+++ app/components/subscribe-form.tsx
@@ -1,14 +1,68 @@
+'use client';
 import styles from "./subscribe-form.module.css";
+import React from "react";
+import { FormEvent, useState } from "react";
+import axios from "axios";
 
-export default async function SubscribeForm() {
+
+const SubscribeForm = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [status, setStatus] = useState<
+        "success" | "error" | "loading" | "idle"
+    >("idle");
+    const [responseMsg, setResponseMsg] = useState<string>("");
+    const [statusCode, setStatusCode] = useState<number>();
+
+    async function handleSubscribe(e: FormEvent<HTMLFormElement>) {
+        console.log("clicked");
+        e.preventDefault();
+        setStatus("loading");
+        try {
+            const response = await axios.post("/api/subscribe", { email });
+
+            setStatus("success");
+            setStatusCode(response.status);
+            setEmail("");
+            setResponseMsg(response.data.message);
+        } catch (err) {
+
+            if (axios.isAxiosError(err)) {
+                setStatus("error");
+                setStatusCode(err.response?.status);
+                setResponseMsg(err.response?.data.error);
+            }
+        }
+        setTimeout(() => {
+            setEmail("");
+            setStatus("idle");
+            setStatusCode(0);
+          }, 10000);
+    }
+
     return (
         <div className={styles.subscribeCont}>
-            <div>
-                <p>Email</p>
-            </div>
-            <div>
-                <p>Subscribe</p>
-            </div>
+            <form className={styles.formCont} onSubmit={handleSubscribe}>
+                <div className={styles.formInputCont}>
+                <input
+                    type="email"
+                    placeholder="email"
+                    className={styles.emailInput} 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={status == "loading"}
+                />
+                <button
+                    type="submit"
+                    className={styles.subButton} 
+                    disabled={status == "loading"}
+                >
+                    Subscribe
+                </button>
+                </div>
+            </form>
         </div>
     );
 }
+
+export default SubscribeForm;
