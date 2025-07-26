7d75ccd6c7cf85b29fecdd7b22e7aa48823bba43
jordan-furr <jordanfurr98@gmail.com>
2025-07-26 16:44:45 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#85b3ff"></meta>
+        <meta name="theme-color" content="#2E2823"></meta>
       </head>
       <Header />
       <body className={`${inter.className} ${inter.className}`}>
