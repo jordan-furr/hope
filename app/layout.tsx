6296367ce854ba3fff3087c82050448ea6625ee2
jordan-furr <jordanfurr98@gmail.com>
2025-07-26 17:17:55 +0200

    social share

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,22 @@ import "./styles/header.css";
 export const metadata: Metadata = {
   title: "Hope Accelerator",
   description: "Retreats in Normandy",
+  openGraph: {
+    title: 'Hope Accelerator',
+    description: 'Immersive retreats run on an ethos: the regenerative power of Nature heals systems and us.',
+    url: 'https://www.hopeaccelerator.com/',
+    siteName: 'Hope Accelerator',
+    images: [
+      {
+        url: 'https://www.hopeaccelerator.com/01.JPG',
+        width: 1200,
+        height: 630,
+        alt: 'Hope Retreats in Normandy',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
