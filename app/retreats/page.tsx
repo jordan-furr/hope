197cfea8511c9559e379b19f5b6b2f1d5fd51524
jordan-furr <jordanfurr98@gmail.com>
2025-10-23 11:18:58 -0400

    payment and decay options

--- app/retreats/page.tsx
+++ app/retreats/page.tsx
@@ -12,13 +12,16 @@ export default function RetreatPage() {
                     <p>Our retreats are designed as living workshops where participants engage with nature's regenerative wisdom. Whether nestled next to our biointensive garden in the landscapes of Normandy, the vibrant energy of Paris, or through our online gatherings, each retreat creates space for deep transformation through the cyclical rhythms that govern all living systems.</p>
                     <h3>Upcoming</h3>
                     <div className='retreat'>
-                        <p className='retreatDates'>April 16-20 & September 24-28, 2026</p>
-                        <p className='retreatTitle'>The Fecundity of Decay: A Yearlong Pilgrimage Towards Selfhood</p>
-                        <p className='retreatSubTitle'>A 4-Night Spring Retreat Honoring Nature’s Rhythms and Your Own</p>
-                         <p><Link href="/retreats/fecundity-of-decay" className="subscribeLink" target='_'>
-                            Discover
+                        <Link href="/retreats/fecundity-of-decay">
+                            <p className='retreatDates'>April 16-20 & September 24-28, 2026</p>
+                            <p className='retreatTitle'>The Fecundity of Decay: A Yearlong Pilgrimage Towards Selfhood</p>
+                            <p className='retreatSubTitle'>A 4-Night Spring Retreat Honoring Nature’s Rhythms and Your Own</p>
+                            <p><Link href="/retreats/fecundity-of-decay" className="subscribeLink">
+                                Discover
+                            </Link>
+                            </p>
                         </Link>
-                        </p>
+
                     </div>
                 </div>
             </div>
