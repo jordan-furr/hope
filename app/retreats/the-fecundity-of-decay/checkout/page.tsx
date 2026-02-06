6ef3b53d1888045416b4ae01abd4a18265a80374
jordan-furr <jordanfurr98@gmail.com>
2026-02-05 20:53:04 -0500

    padding

--- app/retreats/the-fecundity-of-decay/checkout/page.tsx
+++ app/retreats/the-fecundity-of-decay/checkout/page.tsx
@@ -24,8 +24,8 @@ export default function RetreatPage() {
                     </div> */}
                     
                     <div className="buttons dates pt5">
-                        <a className="retreatButton" href="https://book.stripe.com/9B65kvajhbTq2x595i5c40a">April Retreat: 2,000 EUR</a>
-                        <a className="retreatButton" href="https://book.stripe.com/fZu00b9fd2iQfjRgxK5c408">September Retreat: 2,000 EUR</a>
+                        <a className="retreatButton" href="https://book.stripe.com/9B65kvajhbTq2x595i5c40a">Book April 16-20</a>
+                        <a className="retreatButton" href="https://book.stripe.com/fZu00b9fd2iQfjRgxK5c408">Book September 24-28</a>
                     </div>
 
                     <p className="mb4 pt5 dates">We are so excited for you to join us. Please <a href="mailto:info@upschool.org" className="underline">email us</a> with any questions.</p>
