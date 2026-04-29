1a4c80b6c64142243e5e2d4e54e80431d1854b3c
jordan-furr <jordanfurr98@gmail.com>
2026-04-29 14:12:01 -0400

    remove retreats and refresh

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,17 +6,17 @@ export default function AboutPage() {
             <div className='line'></div>
             <div className='pageContent'>
                 <div className='pageTitle'>
-                    Our Story
+                    <h1>About Hope</h1>
                 </div>
-                <div className='mt3 margin-a'>
-                    <p>Hope Accelerator emerged from a simple recognition: that nature's biodynamic systems hold profound insights for healing ourselves and our world. We are a retreat center rooted in place—primarily in Normandy, France—where we explore how the regenerative principles that restore ecosystems can restore human potential.</p>
-                    <h3>Who We Are</h3>
-                    <p className="mv3">Founder: Susannah Harmon Furr</p>
-                    <p className='mv3'>
-                       The Hope Acclerator is one of several life exploration projects run by Susannah and Nathan Robert Furr at {' '}
+                <div className='margin-a'>
+                    <p className='mb3'>The Hope Accelerator emerged from a simple recognition: that nature's biodynamic systems hold profound insights for healing ourselves and our world.</p>
+                    <p className='mb6'>We are a retreat center rooted in place—primarily in Normandy, France—where we explore how the regenerative principles that restore ecosystems can restore human potential.</p>
+                    <p className='bold mb3'>Who We Are</p>
+                    <p className='mb5'>
+                       Founded by Susannah Harmon Furr, the Hope Acclerator is the grounding soil of {' '}
                         <a href="https://upschool.org/" target='_' className="subscribeLink">
-                            UP School.
-                        </a>{' '}
+                            UP School&apos;s
+                        </a>{' '} vision to uplift the world.
                     </p>
                 </div>
             </div>
--- app/retreats/page.tsx
+++ app/retreats/page.tsx
@@ -2,17 +2,19 @@ import Link from 'next/link';
 
 export default function RetreatPage() {
     return (
-        <div className='page'>
+        <div className='page evergreen'>
             <div className='line'></div>
             <div className='pageContent'>
                 <div className='pageTitle'>
-                    <h1>Retreats</h1>
+                    <h1>Retreats in Normandy</h1>
                 </div>
-                <div className='mt3 margin-a'>
-                    <p>Our retreats are designed as living workshops where participants engage with nature's regenerative wisdom. Whether nestled next to our biointensive garden in the landscapes of Normandy, the vibrant energy of Paris, or through our online gatherings, each retreat creates space for deep transformation through the cyclical rhythms that govern all living systems.</p>
-                    <h3>Upcoming</h3>
-                    <div className='retreat'>
-                        <Link href="/retreats/fecundity-of-decay">
+                <div className='margin-a'>
+                    <p className='mb6'>Our retreats are designed as living workshops where participants engage with nature's regenerative wisdom. Whether nestled next to our biointensive garden in the landscapes of Normandy, the vibrant energy of Paris, or through our online gatherings, each retreat creates space for deep transformation through the cyclical rhythms that govern all living systems.</p>
+                    <p className='bold mb3'>Upcoming Retreats</p>
+
+                    <p className='mb3'>We don’t have any retreats scheduled at the moment. Join our mailing list or check back soon.</p>
+                    <Link href="/" className="subscribeLink"><p>Subscribe</p></Link>
+                    {/* <Link href="/retreats/fecundity-of-decay">
                             <p className='retreatDates'>Online Circle: Jan - Dec 2026</p>
                             <p className='retreatDates'>In Person: April 16-20 & September 24-28</p>
                             <p className='retreatTitle'>The Fecundity of Decay: A Yearlong Pilgrimage Towards Selfhood</p>
@@ -21,9 +23,9 @@ export default function RetreatPage() {
                                 Discover
                             </Link>
                             </p>
-                        </Link>
+                        </Link> */}
 
-                    </div>
+
                 </div>
             </div>
         </div>
