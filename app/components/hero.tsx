a02f7df45be399642786915926db3ff15fe6372a
jordan-furr <jordanfurr98@gmail.com>
2026-05-06 22:38:06 -0400

    no compost button

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -12,11 +12,11 @@ export default async function Hero() {
                     className="heroImg"
                 />
             </div>
-            <div className="heroButtonCont">
+            {/* <div className="heroButtonCont">
                     <Link href="/compost" className="heroButton">
                         Compost Notes
                     </Link>
-                </div>
+                </div> */}
         </div>
     );
 }
